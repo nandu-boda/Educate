a24e9ae6eacbc443c7f69a6da771c7619a8d5fc6
nandu <nandub719@gmail.com>
2025-01-24 22:05:38 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,28 @@
-import React from 'react'
-import Navbar from './components/navbar/navbar.jsx'
-import Hero from './components/hero/hero.jsx'
-import Program from './components/programs/program.jsx'
-import Title from './components/title/title.jsx'
-import About from './components/about/about.jsx'
-import Campus from './components/campus/campus.jsx'
+import React from 'react';
+import Navbar from './components/navbar/Navbar.jsx';
+import Hero from './components/hero/Hero.jsx';
+import Program from './components/programs/Program.jsx';
+import Title from './components/title/Title.jsx';
+import About from './components/about/About.jsx';
+import Campus from './components/campus/Campus.jsx';
+import Testimonials from './components/testimonials/testimonails.jsx'
+
 const App = () => {
   return (
     <div>
       <Navbar />
       <Hero />
       <div className='container'>
-        <Title  subTitle = "Our Program" title ="What We Offer"/>
+        <Title subTitle="Our Program" title="What We Offer" />
         <Program />
-        <About/>
-        <Title  subTitle = "Gallery" title ="Campus Photos"/>
+        <About />
+        <Title subTitle="Gallery" title="Campus Photos" />
         <Campus />
+        <Title subTitle="Testimonials" title="What student Says" />
+        <Testimonials />
       </div>
-      
     </div>
-  )
-}
+  );
+};
 
 export default App;
---
+++ src/components/testimonials/testimonails.jsx
@@ -0,0 +1,74 @@
+import React from 'react'
+import './testimonails.css'
+
+const testimonails = () => {
+  return (
+    <div className='testimonials'>
+        <img src="public\back.png" alt=""  className='backbtn'/>
+        <img src="public\right.png" alt="" className='rightbtn' />
+        <div className="slider">
+            <div className="slide">
+                
+                <ul>
+                 <li>
+                    <div className="userinfo">
+                    <img src="public\t1.jpg" alt="" />
+                    <div>
+                        <h3>John Doe</h3>
+                        <span>Educate,USA</span>
+                    </div>
+                    <div>
+                        <p>The university's comprehensive education programs have truly transformed my life. The hands-on learning and personalized mentorship have prepared me to make a meaningful impact in the field of education."</p>
+                    </div>
+                    </div>
+                </li> 
+                  <li>
+                    <div className="userinfo">
+                    <img src="public\t1.jpg" alt="" />
+                    <div>
+                        <h3>John Doe</h3>
+                        <span>Educate,USA</span>
+                    </div>
+                    <div>
+                        <p>The university's comprehensive education programs have truly transformed my life. The hands-on learning and personalized mentorship have prepared me to make a meaningful impact in the field of education."</p>
+                    </div>
+                    </div>
+                </li> 
+                  <li>
+                    <div className="userinfo">
+                    <img src="public\t1.jpg" alt="" />
+                    <div>
+                        <h3>John Doe</h3>
+                        <span>Educate,USA</span>
+                    </div>
+                    <div>
+                        <p>The university's comprehensive education programs have truly transformed my life. The hands-on learning and personalized mentorship have prepared me to make a meaningful impact in the field of education."</p>
+                    </div>
+                    </div>
+                </li> 
+                   <li>
+                    <div className="userinfo">
+                    <img src="public\t1.jpg" alt="" />
+                    <div>
+                        <h3>John Doe</h3>
+                        <span>Educate,USA</span>
+                    </div>
+                    <div>
+                        <p>The university's comprehensive education programs have truly transformed my life. The hands-on learning and personalized mentorship have prepared me to make a meaningful impact in the field of education."</p>
+                    </div>
+                    </div>
+                </li> 
+                </ul>
+                
+              
+                
+                
+            </div>
+            
+        </div>
+        
+    </div>
+  )
+}
+
+export default testimonails;
